dc37715b4154271224aad67260b3fa5560f9e5bc
Manuel Reinhardt <reinhardt@syslab.com>
2016-06-20 13:04:40 +0100

    Added RichText field, refs #13766

--- src/lessons/learned/content/llentry.py
+++ src/lessons/learned/content/llentry.py
@@ -2,12 +2,18 @@
 from plone.supermodel import model
 from zope import schema
 from plone.autoform import directives
+from plone.app.textfield import RichText
 from plone.app.z3cform.widget import AjaxSelectFieldWidget
 
 
 class ILLEntry(model.Schema):
     """ Dexterity Schema for entries to the LL database
     """
+
+    body = RichText(
+        title=(u"Text"),
+        required=False,
+    )
 
     customer = schema.TextLine(
         title=(u'Customer'),
